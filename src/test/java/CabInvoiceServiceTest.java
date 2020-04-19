c5c11902a60c4de481433677e145aeaf738db204
ARATI-UPARE <aratiupare@gmail.com>
2020-04-19 16:43:43 +0530

    ADDED:The Invoice Generator should now take in Multiple Rides,and Calculate the aggregate total for all

--- src/test/java/CabInvoiceServiceTest.java
+++ src/test/java/CabInvoiceServiceTest.java
@@ -1,9 +1,11 @@
 import com.bl.invoicegenerator.InvoiceGenerator.InvoiceGenerator;
+import com.bl.invoicegenerator.InvoiceGenerator.Ride;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class CabInvoiceServiceTest {
 
+    // TC:1
     @Test
     public void givenDistanceAndTime_shouldReturnTotalFare() {
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
@@ -11,5 +13,24 @@ public class CabInvoiceServiceTest {
         int time = 5;
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(25, fare, 0.0);
+    }
+
+    // TC:2.1
+    @Test
+    public void givenLessDistanceOrTime_shouldReturnMiniumFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5, fare, 0.0);
+    }
+    // TC:2.2
+    @Test
+    public void givenMultipleRides_shouldReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        Ride[] rides = { new Ride(2.0, 5),
+                new Ride(0.1, 1)};
+        double fare = invoiceGenerator.calculateFare(rides);
+        Assert.assertEquals(30, fare, 0.0);
     }
 }
