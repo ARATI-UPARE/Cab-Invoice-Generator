16bd39469f827944b0b6524a097db23bcd444d03
ARATI-UPARE <aratiupare@gmail.com>
2020-04-20 09:26:55 +0530

    ADDED:Given a user id, the Invoice Generator gets the List of Rides from the Ride Repository and Returns the Invoice

--- src/test/java/CabInvoiceServiceTest.java
+++ src/test/java/CabInvoiceServiceTest.java
@@ -42,5 +42,16 @@ public class CabInvoiceServiceTest {
         Assert.assertEquals(expectedInvoiceSummary, summary);
     }
 
+    // TC:4
+    @Test
+    public void givenUserIdAndRides_shouldReturnInvoiceSummary() {
+        String userId = "asd";
+        Ride[] rides = { new Ride(2.0, 5),
+                new Ride(0.1, 1)};
+        invoiceGenerator.addRides(userId, rides);
+        InvoiceSummary summary = invoiceGenerator.getInvoiceSummary(userId);
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
+        Assert.assertEquals(expectedInvoiceSummary, summary);
+    }
 
 }
